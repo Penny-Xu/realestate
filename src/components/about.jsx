d16043b67b6e2feb10dc57bac7fbf2ed84924e2e
Penny Xu <pennyxu@Pennys-MacBook-Air.local>
2021-06-06 23:26:18 -0700

    update about and header

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -9,9 +9,10 @@ export const About = (props) => {
           </div>
           <div className='col-xs-12 col-md-6'>
             <div className='about-text'>
-              <h2>About Us</h2>
-              <p>{props.data ? props.data.paragraph : 'loading...'}</p>
-              <h3>Why Choose Us?</h3>
+              <h2>About Penny</h2>
+              <p>{props.data ? props.data.paragraph1 : 'loading...'}</p>
+              <p>{props.data ? props.data.paragraph2 : 'loading...'}</p>
+              <h3>Expertise</h3>
               <div className='list-style'>
                 <div className='col-lg-6 col-sm-6 col-xs-12'>
                   <ul>
